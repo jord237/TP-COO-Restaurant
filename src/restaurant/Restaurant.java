b7c2c7012eaab1f7ff410fe6ab5393633b63b1ba
jordan konga <jordankonga5@gmail.com>
2026-01-28 13:05:47 +0100

    Fin de la classe chercherTableCapaciteSuffisante(int nombrePersonnes)

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -19,6 +19,37 @@ public class Restaurant {
 		Table table = new Table(capacite);
 		tables.add(table);
 	}
+	
+	public boolean tableEstReservee(Table table) {
+		return tablesReservees.containsKey(table);
+	}
+	private Table chercherTableCapaciteSuffisante(int nombrePersonnes) {
+
+	    Table meilleureTable = null;
+
+	    for (Table table : tables) {
+
+	        // La table doit être libre
+	        if (!tableEstReservee(table)) {
+
+	            // La table doit avoir assez de places
+	            if (table.getcapacite() >= nombrePersonnes) {
+
+	                // Première table trouvée
+	                if (meilleureTable == null) {
+	                    meilleureTable = table;
+	                }
+	                // Ou table plus petite que la meilleure actuelle
+	                else if (table.getcapacite() < meilleureTable.getcapacite()) {
+	                    meilleureTable = table;
+	                }
+	            }
+	        }
+	    }
+
+	    return meilleureTable;
+	}
+
 }
 
 	
